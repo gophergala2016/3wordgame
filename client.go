e2c66fbb56df1921bc895a862bd3117bd708b08f
Nic M <nicmalan@gmail.com>
2016-01-23 14:02:00 +0200

    Client take args for server:port with `flag` and has defaults.

--- client.go
+++ client.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"bufio"
-	"net"
+	"flag"
 	"fmt"
+	"net"
 	"os"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", ":6666")
+	var server string
+	var port int
+
+	flag.StringVar(&server, "server", "127.0.0.1", "Server host")
+	flag.IntVar(&port, "port", 6666, "Server port")
+	flag.Parse()
+
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", server, port))
 	if err != nil {
-		fmt.Println("Error dialing in.");
+		fmt.Println("Error dialing in.")
+		os.Exit(-1)
 	}
 
-	fmt.Println("Connected.");
+	fmt.Println("Connected.")
 
 	for {
 		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
