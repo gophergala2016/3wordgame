ae0ac340d446b9d6132c590169a164f2e34eb821
William Stewart <zoidbergwill@gmail.com>
2016-01-23 16:06:00 +0200

    Add basic tests

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ $ go get github.com/gophergala2016/3wordgame
 $ go run server/server.go  # and then in another tab
 $ go run client/client.go
 ```
+
+## Contributing
+
+To run tests for the validation (TODO: running tests form base dir)
+
+```sh
+$ go test github.com/gophergala2016/3wordgame/validation
+```
---
+++ validation/utils_test.go
@@ -0,0 +1,39 @@
+package validation
+
+import (
+	"testing"
+)
+
+func TestValidation(t *testing.T) {
+
+	var msg string
+	var err error
+
+	_, err = ValidateMsg("one two\n")
+	if err == nil {
+		t.Errorf("one two \\n should've failed validation")
+	}
+
+	_, err = ValidateMsg("one two three four\n")
+	if err == nil {
+		t.Errorf("one two three four\\n should've failed validation")
+	}
+
+	msg, err = ValidateMsg("one two three\n")
+	if err != nil {
+		t.Errorf("one two three\\n should've passed validation")
+	}
+
+	if msg != "one two three\n" {
+		t.Errorf("one two three\\n should've stayed that, but instead became %s", msg)
+	}
+
+	msg, err = ValidateMsg("one two  three\n")
+	if err != nil {
+		t.Errorf("one two  three\\n should've passed validation")
+	}
+
+	if msg != "one two three\n" {
+		t.Errorf("one two  three\\n should've become one two three\\n, but instead became %s", msg)
+	}
+}
