248a22b1f231a4473218e72c60ca8c534ef95cd3
Nic M <nicmalan@gmail.com>
2016-01-24 16:37:05 +0200

    dial timeout

--- client/client.go
+++ client/client.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"time"
 )
 
 // var inputChannel = make(chan string)
@@ -18,7 +19,7 @@ func main() {
 	flag.IntVar(&port, "port", 6666, "Server port")
 	flag.Parse()
 
-	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", server, port))
+	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", server, port), time.Second*3)
 	if err != nil {
 		fmt.Println("Error dialing in.")
 		exit()
